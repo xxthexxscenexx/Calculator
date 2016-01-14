1f98e867815f771fc75bef8c41dc0808694f7491
Rosie <Rosie@roses-macbook-pro.local>
2016-01-14 13:56:14 -0500

    Calculator is in working order after first two tutorials. The add/subtract/multiply/divide and square-root buttons have all been placed and are in working order.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    // VARIABLES
+    @IBOutlet weak var display: UILabel! // Display label
+    var userIsInTheMiddleOfTypingANumber: Bool = false // Is the user typing a new number?
+    var operandStack = Array<Double>()
+    var displayValue: Double{
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue // Look up meaning
+        } // end of get
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        } // end of set
+    } // end of displayValue
+    
+    // DIGITS PRESSED
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle // Retrieves the title of the current button pressed
+        // Determine what number to display based on if the user is typing a new number or not
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit!
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        } // end of if else
+        
+    } // end of funct
+    
+    // ENTER BUTTON appends values onto the stack 
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        print("\(operandStack)")
+    } // end of funct
+    
+    // OPERATIONS
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        switch operation {
+            case "✖️": performOperations{ $0 * $1 }
+            case "➗": performOperations{ $1 / $0 }
+            case "➖": performOperations{ $1 - $0 }
+            case "➕": performOperations{ $0 + $1 }
+            case "✔️": performOperation{ sqrt($0) }
+            default:   break
+        }
     }
+    
+    // Perform operation
+    func performOperations(operation: (Double, Double) -> Double){
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        } // end if
+    } // end funct
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}
+    // Perform operation
+    func performOperation(operation: Double -> Double){
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        } // end if
+    } // end funct
+    
+} // end of class
 
